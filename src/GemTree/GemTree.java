dd89afba95951e00fbe743807a6536ad0ba275bd
ImThaDude <jorge_quan@hotmail.com>
2016-01-18 08:49:11 -0800

    First Commit

---
+++ src/GemTree/GemTree.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GemTree;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Jia Yi
+ */
+public class GemTree extends JavaPlugin {
+
+   public void onEnable() {
+       
+   }
+   
+   public void onDisable() {
+       
+   }
+   
+   
+    
+}
